d15261269cfc6f01d33017de5f727881ba884732
Devin Turner <5410284+l1h3r@users.noreply.github.com>
2026-02-03 03:23:35 +0000

    Add shuttle tests (#8)

---
+++ bin/shuttle.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Runs Shuttle tests with defaults for Shuttle's configuration values.
+#
+# The tests are compiled in release mode to improve performance, but debug
+# assertions are enabled.
+#
+# Any arguments to this script are passed to the `cargo test` invocation.
+#
+# Usage:
+#   ./bin/shuttle.sh                # Run all shuttle tests
+#   ./bin/shuttle.sh insert         # Run a specific test
+#   ./bin/shuttle.sh -- --nocapture # See output
+
+set -euo pipefail
+
+RUSTFLAGS="${RUSTFLAGS:-} --cfg shuttle -C debug-assertions=on" \
+    SHUTTLE_SILENCE_WARNINGS="${SHUTTLE_SILENCE_WARNINGS:-1}" \
+    cargo test --features shuttle --release --tests "$@"
